4657fbbed5ceafeff00cbcc1f06e672f334c5f10
mina0774 <mina0774@hanmail.net>
2020-11-21 19:42:45 +0900

    implement XMLImporter

--- HolubSQL/src/com/holub/database/XMLImporter.java
+++ HolubSQL/src/com/holub/database/XMLImporter.java
@@ -1,49 +1,109 @@
 package com.holub.database;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.Iterator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.holub.tools.ArrayIterator;
 
-// todo - 여기부터 시작!!!!
-public class XMLImporter implements Table.Importer{
-	private BufferedReader  in;			// null once end-of-file reached
-	private String[]        columnNames;
-	private String          tableName;
+public class XMLImporter implements Table.Importer {
+	private String[] columnNames;
+	private String tableName;
 
-	public XMLImporter( Reader in ) 
-	{	this.in = in instanceof BufferedReader
-						? (BufferedReader)in
-                        : new BufferedReader(in)
-	                    ;
-	}
-	public void startTable()			throws IOException
-	{	tableName   = in.readLine().trim();
-		columnNames = in.readLine().split("\\s*,\\s*");
-	}
-	public String loadTableName()		throws IOException
-	{	return tableName;
-	}
-	public int loadWidth()			    throws IOException
-	{	return columnNames.length;
-	}
-	public Iterator loadColumnNames()	throws IOException
-	{	return new ArrayIterator(columnNames);  //{=CSVImporter.ArrayIteratorCall}
+	// Get Document Builder
+	DocumentBuilderFactory factory;
+	DocumentBuilder builder;
+	Document document;
+	Element root;
+
+	// 현재 읽고 있는 행번호를 나타내는 
+	int loadRowNum = 0;
+
+	public XMLImporter(File file) throws SAXException, IOException, ParserConfigurationException {
+		this.factory = DocumentBuilderFactory.newInstance();
+		this.builder = factory.newDocumentBuilder();
+
+		document = builder.parse(file);
+		document.getDocumentElement().normalize();
 	}
 
-	public Iterator loadRow()			throws IOException
-	{	Iterator row = null;
-		if( in != null )
-		{	String line = in.readLine();
-			if( line == null )
-				in = null;
-			else
-				row = new ArrayIterator( line.split("\\s*,\\s*"));
+	public void startTable() throws IOException {
+		// table 이름을 얻음
+		root = document.getDocumentElement();
+		tableName = root.getNodeName();
+		// 첫번째 행에서 row tag 이용하여 column의 이름을 가져옴
+		NodeList children = document.getElementsByTagName("row");
+		Node node = children.item(0);
+		Element element = (Element) node;
+		// column의 수를 지정해줌
+		columnNames = new String[element.getAttributes().getLength()];
+		for (int i = 0; i < element.getAttributes().getLength(); i++) {
+			columnNames[i] = element.getAttributes().item(i).getNodeName();
 		}
+	}
+
+	public String loadTableName() throws IOException {
+		return tableName;
+	}
+
+	public int loadWidth() throws IOException {
+		return columnNames.length;
+	}
+
+	public Iterator loadColumnNames() throws IOException {
+		return new ArrayIterator(columnNames);
+	}
+
+	// 하나의 행씩 반환해주는 함수 
+	public Iterator loadRow() throws IOException {
+		Iterator row = null;
+		NodeList children = document.getElementsByTagName("row");
+		
+		//하나의 행에 들어있는 데이터를 받아줄 문자열 배열 
+		String[] tableData = new String[columnNames.length];
+
+		// 행을 다 읽었을 경우 Null을 반환해줌
+		if (loadRowNum == children.getLength()) {
+			return null;
+		}
+
+		// 현재 행을 읽어옴 
+		Node node = children.item(loadRowNum);
+		if (node.getNodeType() == Node.ELEMENT_NODE) {
+			Element element = (Element) node;
+			if (element.getNodeName().equals("row")) {
+				for (int j = 0; j < columnNames.length; j++) {
+					tableData[j] = element.getAttribute(columnNames[j]);
+				}
+				row = new ArrayIterator(tableData);
+			}
+		}
+		
+		// 현재 읽고 있는 행의 수를 증가시켜줌 
+		loadRowNum++;
+
 		return row;
 	}
 
-	public void endTable() throws IOException {}
+	public void endTable() throws IOException {
+	}
+
+	public static class Test {
+		public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+			File file = new File("/Users/jomin-a/git/2020CAU_Design_Pattern/HolubSQL/testdata/people.xml");
+			Table people = new ConcreteTable(new XMLImporter(file));
+
+			System.out.println(people.toString());
+		}
+	}
 }
